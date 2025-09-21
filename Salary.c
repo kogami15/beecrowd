451f93860ee54e17eca1216cc9b08befd41a2c97
kogami15 <apsarikamrun22@gmail.com>
2025-09-21 17:59:59 +0600

    Create Salary.c

---
+++ Salary.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int A,B;
+    double C,D;
+    scanf("%d%d%lf", &A, &B, &C);
+    D=B*C;
+    printf("NUMBER = %d\nSALARY = U$ %.2lf\n",A,D);
+    
+    return 0;
+}
