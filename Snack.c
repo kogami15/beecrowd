7fa4429cee893f29a86e0c54de8dc2c2f19dbce8
kogami15 <apsarikamrun22@gmail.com>
2025-09-21 18:14:21 +0600

    Create Snack.c

---
+++ Snack.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int X,Y;
+    float T;
+    scanf("%d%d",&X,&Y);
+    if(X==1){
+            T=Y*4.00;
+            printf("Total: R$ %.2f\n",T);
+    }
+    else if(X==2){
+            T=Y*4.50;
+            printf("Total: R$ %.2f\n",T);
+    }
+    else if(X==3){
+            T=Y*5.00;
+            printf("Total: R$ %.2f\n",T);
+    }
+    else if(X==4){
+            T=Y*2.00;
+            printf("Total: R$ %.2f\n",T);
+    }
+    else{
+            T=X*1.50;
+            printf("Total: R$ %.2f\n",T);
+    }
+    return 0;
+
+}
